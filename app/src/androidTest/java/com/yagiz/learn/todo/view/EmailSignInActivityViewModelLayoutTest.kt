62b668aa18dc518652814d018fdca3c7998fc506
Yağız Şenal <yagizsenal@gmail.com>
2019-02-09 20:08:51 +0300

    EmailSignInActivityViewModel Unit and Layout tests added.

---
+++ app/src/androidTest/java/com/yagiz/learn/todo/view/EmailSignInActivityViewModelLayoutTest.kt
@@ -0,0 +1,76 @@
+package com.yagiz.learn.todo.view
+
+import android.support.test.espresso.Espresso
+import android.support.test.espresso.Espresso.onView
+import android.support.test.espresso.action.ViewActions.*
+import android.support.test.espresso.assertion.ViewAssertions.matches
+import android.support.test.espresso.matcher.ViewMatchers.*
+import android.support.test.runner.AndroidJUnit4
+import com.yagiz.learn.todo.BR
+import com.yagiz.learn.todo.BaseLayoutTest
+import com.yagiz.learn.todo.R
+import com.yagiz.learn.todo.databinding.ActivitySigninBinding
+import org.hamcrest.CoreMatchers.`is`
+import org.hamcrest.CoreMatchers.not
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.Mockito.mock
+import org.mockito.Mockito.verify
+
+@RunWith(AndroidJUnit4::class)
+class EmailSignInActivityViewModelLayoutTest : BaseLayoutTest<ActivitySigninBinding, EmailSignInActivityViewModel>() {
+    private lateinit var viewModel: EmailSignInActivityViewModel
+    private lateinit var navigator: ISignInNavigator
+
+    @Before
+    fun setup() {
+        navigator = mock(ISignInNavigator::class.java)
+        viewModel = EmailSignInActivityViewModel(navigator)
+    }
+
+    @Test
+    fun nullViewModelTest() {
+        setLayout(R.layout.activity_signin, BR.viewModel, null)
+
+        onView(withId(R.id.et_email)).check(matches(withText("")))
+        onView(withId(R.id.et_password)).check(matches(withText("")))
+        onView(withId(R.id.btn_signin)).check(matches(not(isEnabled())))
+    }
+
+    @Test
+    fun bindEmailTest() {
+        setLayout(R.layout.activity_signin, BR.viewModel, viewModel)
+        onView(withId(R.id.et_email)).perform(replaceText("Email"))
+
+        assertThat(viewModel.email.get(), `is`("Email"))
+
+    }
+
+    @Test
+    fun bindPasswordTest() {
+        setLayout(R.layout.activity_signin, BR.viewModel, viewModel)
+        onView(withId(R.id.et_password)).perform(replaceText("Password"))
+
+        assertThat(viewModel.password.get(), `is`("Password"))
+    }
+
+    @Test
+    fun signInButtonEnabledBindTest() {
+        setLayout(R.layout.activity_signin, BR.viewModel, viewModel)
+        onView(withId(R.id.et_email)).perform(click(), typeText("Email"))
+        onView(withId(R.id.et_password)).perform(click(), typeText("Password"))
+        onView(withId(R.id.btn_signin)).check(matches(isEnabled()))
+    }
+
+    @Test
+    fun clickSignInButtonTest() {
+        setLayout(R.layout.activity_signin, BR.viewModel, viewModel)
+        onView(withId(R.id.et_email)).perform(click(), typeText("Email"))
+        onView(withId(R.id.et_password)).perform(click(), typeText("Password"))
+        Espresso.closeSoftKeyboard()
+        onView(withId(R.id.btn_signin)).perform(click())
+        verify(navigator).signInWithEmail("Email", "Password")
+    }
+
+}
---
+++ app/src/test/java/com/yagiz/learn/todo/view/EmailSignInActivityViewModelTest.kt
@@ -0,0 +1,42 @@
+package com.yagiz.learn.todo.view
+
+import android.databinding.ObservableField
+import org.hamcrest.CoreMatchers.`is`
+import org.hamcrest.MatcherAssert.assertThat
+import org.junit.Before
+import org.junit.Test
+import org.mockito.Mockito.mock
+
+class EmailSignInActivityViewModelTest {
+
+    private lateinit var navigator: ISignInNavigator
+    private lateinit var viewModel: EmailSignInActivityViewModel
+
+    @Before
+    fun setup() {
+        navigator = mock(ISignInNavigator::class.java)
+        viewModel = EmailSignInActivityViewModel(navigator)
+    }
+
+
+    @Test
+    fun setEmailTest() {
+        viewModel.email = ObservableField("Email")
+        assertThat(viewModel.email.get(), `is`("Email"))
+    }
+
+    @Test
+    fun setPasswordTest() {
+        viewModel.password = ObservableField("Password")
+        assertThat(viewModel.password.get(), `is`("Password"))
+    }
+
+    @Test
+    fun setSignInButtonEnabledTest() {
+        viewModel.email = ObservableField("Email")
+        viewModel.password = ObservableField("Password")
+        assertThat(viewModel.signInButtonEnabled.get(), `is`(true))
+    }
+
+}
+
