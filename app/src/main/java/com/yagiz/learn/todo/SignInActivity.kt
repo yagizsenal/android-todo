759be76eeaa6b07b6cc4308ccda81fb2c7750a7f
Yağız Şenal <yagizsenal@gmail.com>
2019-02-10 21:44:32 +0300

    Added sign in functionality.

--- app/src/main/java/com/yagiz/learn/todo/SignInActivity.kt
+++ app/src/main/java/com/yagiz/learn/todo/SignInActivity.kt
@@ -1,12 +1,51 @@
 package com.yagiz.learn.todo
 
+import android.content.Intent
+import android.databinding.DataBindingUtil
 import android.os.Bundle
-import android.os.PersistableBundle
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.yagiz.learn.todo.databinding.ActivitySigninBinding
+import com.yagiz.learn.todo.navigator.ISignInNavigator
+import com.yagiz.learn.todo.view.EmailSignInActivityViewModel
 
-class SignInActivity : AppCompatActivity(){
+class SignInActivity : AppCompatActivity(), ISignInNavigator {
 
-    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
-        super.onCreate(savedInstanceState, persistentState)
+    private val TAG = "SignInActivity"
+
+    private lateinit var auth: FirebaseAuth
+    override fun signInWithEmail(email: String, password: String) {
+        auth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        proceedToMainActivity()
+                    } else {
+                        Log.w(TAG, "createUserWithEmail:failure", task.exception)
+                        Toast.makeText(baseContext, task.exception?.message ?: "Authentication failed", Toast.LENGTH_SHORT).show()
+                    }
+                }
+    }
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        auth = FirebaseAuth.getInstance()
+        if (auth.currentUser != null) {
+            proceedToMainActivity()
+            return
+        }
+
+        val viewModel = EmailSignInActivityViewModel(this)
+        val binding = DataBindingUtil.setContentView<ActivitySigninBinding>(this, R.layout.activity_signin)
+        binding.viewModel = viewModel
+        binding.executePendingBindings()
+    }
+
+    private fun proceedToMainActivity(){
+        startActivity(Intent(this, MainActivity::class.java))
+        finish()
     }
 }
