2ac3d7d73b9db248eadece9c294dc5c58ba097e2
Yağız Şenal <yagizsenal@gmail.com>
2019-02-10 21:45:05 +0300

    Added Firestore integration.

--- app/src/main/java/com/yagiz/learn/todo/MainActivity.kt
+++ app/src/main/java/com/yagiz/learn/todo/MainActivity.kt
@@ -1,12 +1,36 @@
 package com.yagiz.learn.todo
 
-import android.support.v7.app.AppCompatActivity
+import android.arch.lifecycle.Observer
+import android.databinding.DataBindingUtil
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import com.yagiz.learn.todo.databinding.ListTaskBinding
+import com.yagiz.learn.todo.model.TaskItem
+import com.yagiz.learn.todo.navigator.ITaskNavigator
+import com.yagiz.learn.todo.provider.TaskItemProvider
+import com.yagiz.learn.todo.view.ItemListScreenViewModel
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), ITaskNavigator {
+    private val TAG= "MainActivity"
+    override fun openEditTaskFragment(task: TaskItem) {
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val tasks = TaskItemProvider().getTasks()
+        val viewModel = ItemListScreenViewModel(this)
+        viewModel.setModel(listOf(TaskItem("Initial","Item",false)))
+        val binding = DataBindingUtil.setContentView<ListTaskBinding>(this, R.layout.list_task)
+        tasks.observe(this, Observer {
+            if (it != null) {
+                Log.d(TAG,"Updating viewModel")
+                binding.viewModel?.setModel(it)
+                binding.executePendingBindings()
+            }
+        })
+        binding.setLifecycleOwner(this)
+        binding.viewModel = viewModel
+        binding.executePendingBindings()
     }
 }
---
+++ app/src/main/java/com/yagiz/learn/todo/provider/TaskItemProvider.kt
@@ -0,0 +1,30 @@
+package com.yagiz.learn.todo.provider
+
+import android.arch.lifecycle.MutableLiveData
+import android.util.Log
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.yagiz.learn.todo.model.TaskItem
+
+class TaskItemProvider {
+    private val TAG = "TaskItemProvider"
+
+    fun getTasks(): MutableLiveData<List<TaskItem>> {
+        val user = FirebaseAuth.getInstance().currentUser
+        val db = FirebaseFirestore.getInstance()
+        val results = MutableLiveData<List<TaskItem>>()
+        db.collection("users").document(user!!.uid)
+                .get()
+                .addOnSuccessListener { result ->
+                    val data = mutableListOf<TaskItem>()
+                    val tasks = result.data!!["tasks"] as ArrayList<*>
+                    for (i in 0..(tasks.size - 1)) {
+                        val task = tasks[i] as HashMap<String, Any>
+                        data.add(TaskItem(task["title"] as String, task["subtitle"] as String, task["completed"] as Boolean))
+                        Log.d(TAG, "Data: $data")
+                    }
+                    results.postValue(data)
+                }
+        return results
+    }
+}
