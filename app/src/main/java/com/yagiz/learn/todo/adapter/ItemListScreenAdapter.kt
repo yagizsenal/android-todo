674307b2fa5df0a639120d8d3ce0a33f9c61d1ff
Yağız Şenal <yagizsenal@gmail.com>
2019-02-19 00:24:29 +0300

    Solved the issue where the tasks are not being updated.

--- app/src/main/java/com/yagiz/learn/todo/adapter/ItemListScreenAdapter.kt
+++ app/src/main/java/com/yagiz/learn/todo/adapter/ItemListScreenAdapter.kt
@@ -4,18 +4,18 @@ import android.support.v7.widget.RecyclerView
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import com.yagiz.learn.todo.MainActivity
 import com.yagiz.learn.todo.databinding.ItemTaskBinding
 import com.yagiz.learn.todo.model.TaskItem
 import com.yagiz.learn.todo.navigator.ITaskNavigator
 import com.yagiz.learn.todo.view.TaskItemViewModel
 
-class ItemListScreenAdapter(private val navigator: ITaskNavigator) : RecyclerView.Adapter<ItemListScreenAdapter.TodoItemViewHolder>() {
+class ItemListScreenAdapter(private val navigator: ITaskNavigator) : RecyclerView.Adapter<ItemListScreenAdapter.TaskItemViewHolder>() {
     private val TAG = "ItemListScreenAdapter"
     private var dataset: List<TaskItem> = listOf()
 
-    override fun onBindViewHolder(viewHolder: TodoItemViewHolder, position: Int) {
+    override fun onBindViewHolder(viewHolder: TaskItemViewHolder, position: Int) {
         with(viewHolder.binding) {
-            if (this.viewModel == null) this.viewModel = TaskItemViewModel(navigator)
             this.viewModel?.setModel(dataset[position])
             executePendingBindings()
         }
@@ -29,23 +29,14 @@ class ItemListScreenAdapter(private val navigator: ITaskNavigator) : RecyclerVie
 
     override fun getItemCount(): Int = dataset.size
 
-    override fun onCreateViewHolder(parent: ViewGroup, type: Int): TodoItemViewHolder {
+    override fun onCreateViewHolder(parent: ViewGroup, type: Int): TaskItemViewHolder {
         val inflater = LayoutInflater.from(parent.context)
-        val binding = ItemTaskBinding.inflate(inflater, parent, false)
-        return TodoItemViewHolder(binding)
-    }
-
-    class TodoItemViewHolder(val binding: ItemTaskBinding) :
-            RecyclerView.ViewHolder(binding.root) {
-
-        fun bind(viewModel: TaskItemViewModel) {
-
-            with(binding) {
-                this.viewModel = viewModel
-                executePendingBindings()
-            }
+        val binding = ItemTaskBinding.inflate(inflater, parent, false).apply {
+            viewModel = TaskItemViewModel(navigator)
+            setLifecycleOwner(parent.context as MainActivity)
         }
-
+        return TaskItemViewHolder(binding)
     }
 
+    class TaskItemViewHolder(val binding: ItemTaskBinding) : RecyclerView.ViewHolder(binding.root)
 }
